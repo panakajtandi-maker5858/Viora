f18d8994bbfc88bf5d1f120edf268fbf8ba89a39
panakajtandi-maker5858 <panakajtandi@gmail.com>
2026-05-11 13:05:46 +0530

    chore: install and update frontend project dependencies in node_modules

---
+++ Frontend/src/app/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import { RouterProvider} from 'react-router'
+import { routes } from './app.routes'
+
+
+const App = () => {
+  return (
+  
+   <>
+      <RouterProvider router={routes} />
+    </>
+
+  )
+}
+
+export default App
