501dfb68ca9aff0096a40eacb5ed3a909a51bd80
panakajtandi-maker5858 <panakajtandi@gmail.com>
2026-05-13 07:12:29 +0530

    feat: implement seller dashboard page and add routing for product management

--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import Register from "../features/auth/pages/Register";
 import Login from "../features/auth/pages/Login";
 import CreateProduct from "../features/products/pages/CreateProduct";
+import Dashboard from "../features/products/pages/Dashboard";
+import Protected from "../features/auth/components/Protected";
+import AppLayout from "./Applayout";
 
 export const routes = createBrowserRouter([
     {
@@ -12,12 +15,30 @@ export const routes = createBrowserRouter([
         path: "/login",
         element: <Login />,
     },
-    {
-        path: "/",
-        element: <div>Home Page</div>,
-    },
-    {
-        path: "/seller/create-product",
-        element: <CreateProduct />
-    },
+   {
+    element: <AppLayout/> ,
+    children: [
+        {
+            path: "/",
+            element: <div>Home Page</div>
+        } ,
+        {
+            path: "/seller" ,
+            children: [
+                {
+                    path: "/seller/create-product",
+                    element: <Protected role="seller">
+                        <CreateProduct/>
+                    </Protected>
+                } ,
+                {
+                    path: "/seller/dashboard",
+                    element: <Protected role="seller">
+                        <Dashboard/>
+                    </Protected>
+                }
+            ]
+        }
+    ]
+   }
 ])
