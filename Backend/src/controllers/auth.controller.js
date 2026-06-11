b673e10d12fb7ae67839d29031749dc8bc5e9be1
panakajtandi-maker5858 <panakajtandi@gmail.com>
2026-06-11 17:31:36 +0530

    fix: update cookie options for production cross-domain auth

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,17 +2,23 @@ import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken"
 import { config } from "../config/config.js";
 
+const isProduction = config.NODE_ENV === 'production'
 
+const cookieOptions = {
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    sameSite: isProduction ? 'none' : 'lax',
+    secure: isProduction ? true : false,
+}
 
 async function sendTokenResponse(user, res, message) {
-
     const token = jwt.sign({
         id: user._id,
     }, config.JWT_SECRET, {
         expiresIn: "7d"
     })
 
-    res.cookie("token", token)
+    res.cookie("token", token, cookieOptions)
 
     res.status(200).json({
         message,
@@ -27,16 +33,12 @@ async function sendTokenResponse(user, res, message) {
     })
 }
 
-
 export const register = async (req, res) => {
     const { email, contact, password, fullname, isSeller } = req.body;
 
     try {
         const existingUser = await userModel.findOne({
-            $or: [
-                { email },
-                { contact }
-            ]
+            $or: [{ email }, { contact }]
         })
 
         if (existingUser) {
@@ -77,34 +79,29 @@ export const login = async (req, res) => {
     await sendTokenResponse(user, res, "User logged in successfully")
 }
 
+export const googleCallback = async (req, res) => {
+    const { id, displayName, emails, photos } = req.user
+    const email = emails[0].value;
+    const profilePic = photos[0].value;
+    let user = await userModel.findOne({ email })
 
-export const googleCallback = async (req , res)=>{
-    const { id , displayName , emails , photos } = req.user 
-   const email = emails[0].value ;
-   const profilePic = photos[0].value ;
-   let user  = await userModel.findOne({ email })
+    if (!user) {
+        user = await userModel.create({
+            email,
+            googleId: id,
+            fullname: displayName
+        })
+    }
 
-  if(!user){
-    user = await userModel.create({
-        email , 
-        googleId: id ,
-        fullname : displayName 
+    const token = jwt.sign({
+        id: user._id,
+    }, config.JWT_SECRET, {
+        expiresIn: "7d"
     })
-  }
 
-  const token = jwt.sign({
-    id : user._id ,
-  }, config.JWT_SECRET , {
-    expiresIn: "7d"
-  })
-
-res.cookie("token" , token)
-res.redirect("http://localhost:5173/")
-
-
+    res.cookie("token", token, cookieOptions)
+    res.redirect(isProduction ? "https://viora-mu.vercel.app/" : "http://localhost:5173/")
 }
-
-
 
 export const getMe = async (req, res) => {
     const user = req.user;
@@ -121,4 +118,3 @@ export const getMe = async (req, res) => {
         }
     })
 }
-
